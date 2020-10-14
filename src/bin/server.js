98c9d9e0ca140f66127dfbab532e6e94fdd1dbe8
puneet2081993 <puneet2081993@gmail.com>
2020-10-14 22:35:10 +0530

    creating and starting server

---
+++ src/bin/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config({path:'../rewaa-backend/.env'})
+const http = require('http')
+const {port} = require('../vars')
+var cluster = require('cluster')
+
+const server = http.createServer() // including express middleware
+const numCPUs = require('os').cpus().length // cpu cores
+
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+
+  //assigning workers for increasing request.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+    //starting server
+    server.listen(port,
+        ()=>{
+            console.log(`Server started running on port ${port}`)
+        }
+    )
+}
---
+++ src/vars.js
@@ -0,0 +1,7 @@
+let node_env = process.NODE_ENV
+let port = process.env.PORT
+
+module.exports = {
+    env : node_env, 
+    port : port
+}
